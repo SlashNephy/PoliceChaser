1a674f0f6a064f05185bad6f1801eaad31a03e7f
Nep <spica@starry.blue>
2016-11-11 14:27:27 +0900

    Improve: more information

--- script/PoliceChaser.lua
+++ script/PoliceChaser.lua
@@ -1,14 +1,4 @@
-require("lib/managers/groupaimanager")
-local _update_original = GroupAIManager.update
-
-local previous_phase
-
-function isSupportedPhase(phase)
-    if phase == "anticipation" or phase == "fade" or phase == "build" or phase == "sustain" then
-        return true
-    end
-    return false
-end
+local text_original = LocalizationManager.text
 
 function getFormattedPhase(phase)
     if phase == "anticipation" then
@@ -20,22 +10,43 @@ function getFormattedPhase(phase)
     elseif phase == "sustain" then
         return "Sustain"
     end
-    return "Unknown"
+    return string.format("Unknown (%s)", phase)
 end
 
-function GroupAIManager:update(...)
-    _update_original(self, ...)
+function updatePhase()
+    if managers.chat then
+        local groupaistate = managers.groupai:state()
+        if groupaistate:get_hunt_mode() then
+            local text = "This assault is Endless mode."
+        else
+            local phase = getFormattedPhase(groupaistate._task_data.assault.phase)
 
-    if self._state_name == "besiege" and managers.chat then
-        local phase = getFormattedPhase(self._state._task_data.assault.phase)
-
-        if phase and phase ~= previous_phase then
-            previous_phase = phase
-
-            if isSupportedPhase(phase) then
-                local text = string.format("Current Assault Phase: %s", phase)
-                managers.chat:feed_system_message(ChatManager.GAME, text)
+            local spawns = groupaistate:_get_difficulty_dependent_value(tweak_data.group_ai.besiege.assault.force_pool) * groupaistate:_get_balancing_multiplier(tweak_data.group_ai.besiege.assault.force_pool_balance_mul)
+            if spawns > 0 then
+                local spawnleftText = string.format("%d spawns left", spawns - groupaistate._task_data.assault.force_spawned)
+            else
+                local spawnleftText = "No more spawnable"
             end
+
+            local phaseTime = groupaistate._task_data.assault.phase_end_t + math.lerp(groupaistate:_get_difficulty_dependent_value(tweak_data.group_ai.besiege.assault.sustain_duration_min), groupaistate:_get_difficulty_dependent_value(tweak_data.group_ai.besiege.assault.sustain_duration_max), math.random()) * groupaistate:_get_balancing_multiplier(tweak_data.group_ai.besiege.assault.sustain_duration_balance_mul) + tweak_data.group_ai.besiege.assault.fade_duration * 2
+            if phaseTime > 0 then
+                local timeText = string.format("%.2f secs left", atime + 350 - groupaistate._t)
+            else
+                local timeText = "This phase has finished."
+            end
+
+            local waveText = string.format("Wave %d", groupaistate._wave_counter or 0)
+
+            local text = string.format("Assault Phase: %s (%s) / %s / %s", phase, waveText, spawnleftText, timeText)
         end
+
+        managers.chat:feed_system_message(ChatManager.GAME, text)
     end
+end
+
+function LocalizationManager:text(string_id, ...)
+	if string_id == "hud_assault_enhanced" then
+        updatePhase()
+	end
+	return text_original(self, string_id, ...)
 end
